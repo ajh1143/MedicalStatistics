9218e295e15361ba2b778b7ddcd959577c9038ad
Andrew Holt <ajh.206@outlook.com>
2019-01-16 11:45:17 -0800

    Update 3.6_Exercises.py

--- Chapter_3/3.6_Exercises.py
+++ Chapter_3/3.6_Exercises.py
@@ -21,45 +21,64 @@ def exercise_i(data):
     """
     :param data: list of age of expiration due to motorcycle accident
     """
-        def get_counts(data_to_count):
-            """
-            Fetch Key(Age), Value(Counts) pairs for expirations per age entry
-            """
-            counted_data = Counter(data_to_count)
-            factors = list(counted_data.keys())
-            counts = list(counted_data.values())
-            return factors, counts
 
-    # Get counts
+    def get_counts(data_to_count):
+    """counts of incident per age"""
+        counted_data = Counter(data_to_count)
+        factors = list(counted_data.keys())
+        counts = list(counted_data.values())
+        return factors, counts
+
+    # Get counts of incident per age
     factors, counts = get_counts(data)
-    
-    # Build Dot Plot
+
+    #Create Dot Plot
     dot_plot = figure(title="Ages of Motorcyclists Critically Injured", tools="hover", toolbar_location=None,
                       y_range=[0,max(counts)+1],  x_range=[0, 100])
     dot_plot.circle(y=counts,x=factors, size=10, fill_color="red", line_color="black", line_width=3)
 
-    #Convert data to DataFrame for MatPlotLib Histogram
-    df = pd.DataFrame(data)
 
+    #Convert data to DataFrame
+    df = pd.DataFrame(data)
     # Create bin, ages 0 to 100, intervals of 5
     bin_range = range(0,100,5)
-    
-    # Construct Histogram Plot, apply a grid, set bins, remove unnecessary legend
-    df.plot.hist(grid=True, bins=bin_range,
-                       color='#607c8e', legend=False)
-    
-    # Add title, x and y axis labels
-    plt.title("Ages of Motorcyclists Critically Injured")
-    plt.xlabel("Ages")
-    plt.ylabel("Counts")
-    
-    # Apply X-axis ticks, 5 year ranges
-    plt.xticks(bin_range)
 
+    # Construct Histogram Plot, apply a grid, set bins, remove unnecessary legend
+    def get_histogram(dataframe):
+        dataframe.plot.hist(grid=True, bins=bin_range,
+                           color='#607c8e', legend=False)
+        # Add title, x and y axis labels
+        plt.title("Ages of Motorcyclists Critically Injured")
+        plt.xlabel("Ages")
+        plt.ylabel("Counts")
+        # Apply X-axis ticks, 5 year ranges
+        plt.xticks(bin_range)
+        plt.show()
+
+    def get_kde_unshaded(dataframe):
+        # KDE Plot, set line color to red, constrain distribution from 0 to 100
+        dataframe.plot(kind='kde', color='red', legend=False).set_xlim(0,100)
+        plt.title("Ages of Motorcyclists Critically Injured")
+        plt.xlabel("Ages")
+        plt.ylabel('Density')
+        # Apply X-axis ticks, 5 year ranges
+        plt.xticks(bin_range)
+        plt.show()
+
+    def get_kde_shaded(dataframe):
+        ax = sns.distplot(dataframe,hist=False, kde=True,
+                     kde_kws={'shade': True, 'linewidth': 3})
+        ax.set_title('Ages of Motorcyclists Critically Injured')
+        ax.set_ylabel('Density')
+        ax.set_xlabel("Age")
+        ax.set_xlim(0,100)
+
+    get_histogram(df)
+    get_kde_unshaded(df)
+    get_kde_shaded(df)
     # Generate plots
     plt.show()
     show(dot_plot)
-
 def exercise_ii(data):
     data_mean =np.mean(data)
     data_median = np.median(data)
